f2b6ba4304176074d0db2aa8fa805cb3cc20031e
Yoru <hoanghuynhtuankiet69@gmail.com>
2024-01-18 16:38:38 +0700

    #13 - yoru integrate cache manager and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,21 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmark API')
+    .setDescription(
+      'The Bookmark API provides a set of endpoints for users to manage bookmarks. Users can perform authentication to securely access their bookmarked resources. The API supports standard CRUD operations for managing bookmarks, allowing users to create, retrieve, update, and delete bookmarks.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
